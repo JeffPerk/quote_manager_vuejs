ce2fbd563050dfeabe371653435ae3186fbcf495
Jeff Perkins <jeff.perk87@gmail.com>
2018-03-08 10:03:47 -0700

    Added delete functionality to quote

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
         <div>
-            <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
+            <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</app-quote>  <!-- the @click.native modifier allows the behavior of the single quote be functional on the parent  -->
         </div>
     </div>
 </template>
@@ -13,6 +13,11 @@
       props: ['quotes'],
       components: {
         appQuote: Quote
+      },
+      methods: {
+        deleteQuote(index) {
+          this.$emit('quoteDeleted', index);
+        }
       }
     }
 </script>
